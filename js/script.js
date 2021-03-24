ce71f58f120d65d6c71a46a6a3797e448c4c0e03
J0nath6n <tuflya007@gmail.com>
2021-03-24 22:30:49 +0200

    add sections header and about + add images and fonts

---
+++ js/script.js
@@ -0,0 +1,20 @@
+//carousel
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1
+            },
+
+            1000:{
+                items:1
+            }
+        }
+    });
+});
